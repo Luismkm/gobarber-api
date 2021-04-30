25a25ee65e0f70f0c514427b62f4a024fa33f580
Luismkm <luismarcelo13@yahoo.com.br>
2021-04-29 21:06:38 -0300

    implementing list of available days

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,7 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Raw } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 
 import Appointment from '../entities/Appointment';
 
@@ -18,6 +19,25 @@ class AppointmentsRepository implements IAppointmentsRepository {
     });
 
     return findAppointment;
+  }
+
+  public async findAllInMonthProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthProviderDTO): Promise<Appointment[]> {
+    const parseMonth = String(month).padStart(2, '0');
+
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(
+          dateFieldname =>
+            `to_char(${dateFieldname}, 'MM-YYYY') = '${parseMonth}-${year}'`,
+        ),
+      },
+    });
+    return appointments;
   }
 
   public async create({
